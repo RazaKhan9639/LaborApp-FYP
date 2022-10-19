85d4f5d711d36bdc7f1481d6db5ddd09b54db650
RazaKhan <raza.khan4336@gmail.com>
2022-10-19 22:11:51 +0100

    Home Page First Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 // import Login from "./components/Login/Login";
 // import SignUp from "./components/SignUp/SignUp";
-import SignInOutContainer from "./containers";
+// import SignInOutContainer from "./containers";
+import HomeFirstSection from "./components/HomePage/HomePage1/HomeFirstSection";
 function App() {
   return (
     <>
       <div className="App">
-        <SignInOutContainer />
+        {/* <SignInOutContainer /> */}
+        <HomeFirstSection />
       </div>
     </>
   );
