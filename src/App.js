d6391168864717722e264920969fd5afd6933849
RazaKhan <raza.khan4336@gmail.com>
2022-10-20 15:13:10 +0100

    Home Page1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 // import Login from "./components/Login/Login";
 // import SignUp from "./components/SignUp/SignUp";
-// import SignInOutContainer from "./containers";
-import HomeFirstSection from "./components/HomePage/HomePage1/HomeFirstSection";
+import SignInOutContainer from "./containers";
+// import HomeFirstSection from "./components/HomePage/HomePage1/HomeFirstSection";
 function App() {
   return (
     <>
       <div className="App">
-        {/* <SignInOutContainer /> */}
-        <HomeFirstSection />
+        <SignInOutContainer />
+        {/* <HomeFirstSection /> */}
       </div>
     </>
   );
