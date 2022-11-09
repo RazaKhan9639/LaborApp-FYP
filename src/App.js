f269137c3197e4bf5d3532cb1363eecafec90a54
RazaKhan <raza.khan4336@gmail.com>
2022-11-09 23:15:57 +0000

    Add Routing to App Js component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
-// import Login from "./components/Login/Login";
-// import SignUp from "./components/SignUp/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 import SignInOutContainer from "./containers";
-// import HomeFirstSection from "./components/HomePage/HomePage1/HomeFirstSection";
+import HomeFirstSection from "./components/HomePage/HomePage1/HomeFirstSection";
 function App() {
   return (
     <>
       <div className="App">
-        <SignInOutContainer />
-        {/* <HomeFirstSection /> */}
+        {/* <SignInOutContainer />
+        <HomeFirstSection /> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<SignInOutContainer />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+            <Route path="/home" element={<HomeFirstSection />} />
+          </Routes>
+        </Router>
       </div>
     </>
   );
